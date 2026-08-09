23097da434d91710f11a73ec11291b9ab49a5b59
Masike <162551252+Masike16@users.noreply.github.com>
2026-08-09 20:34:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/centralink/MainActivity.kt
@@ -0,0 +1,13 @@
+// ST10452404 OPSC6312 ICE TASK 1
+package com.example.centralink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
